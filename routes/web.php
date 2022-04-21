8d5fc57920f1945b13cb126eca38d400ef5a748e
Tanti Wulansari <tantiwulansari2603@gmail.com>
2022-04-21 10:13:19 +0100

    tanti commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('login');
 });
+
+Route::get('/lowongan', function () {
+    return view('user.lowongan_kerja_pelamar');
+});
