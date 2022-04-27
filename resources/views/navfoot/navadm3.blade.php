97204a15a6c64abc381abebcfcae1b5152ec8573
ardianhilmip35 <ardianhil86@gmail.com>
2022-04-27 20:17:27 +0700

    hilmi commit

--- resources/views/navfoot/navadm3.blade.php
+++ resources/views/navfoot/navadm3.blade.php
@@ -1,24 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>@yield('title')</title>
-        <!--Bootstrap css-->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
-        <!-- custom css  -->
-        <link rel="stylesheet" href="/style.css">
-        <style>
-            body {
-                font-family: 'Poppins', sans-serif;
-            }
-        </style>
-    </head>
-<body style="background-color: #F1F1F1">
+@include('layouts.header')
     
 {{-- Awal Navbar --}}
 <body>
