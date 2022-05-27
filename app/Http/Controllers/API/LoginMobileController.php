e40b9a2f492b6750235bd3144e597d270c381355
ardianhilmip35 <ardianhil86@gmail.com>
2022-05-27 20:30:17 +0700

    hilmi commit

--- app/Http/Controllers/API/LoginMobileController.php
+++ app/Http/Controllers/API/LoginMobileController.php
@@ -9,18 +9,24 @@ use Illuminate\Support\Facades\Hash;
 
 class LoginMobileController extends Controller
 {
-    // public function login(Request $request){
-    //     $request->validate([
-    //         'email' => 'required|email',
-    //         'password' => 'required|string',
-    //     ]);
-    //     $user = UserMobile::where('email', $request->email)->first();
-    //     if(! $user || ! Hash::check($request->password, $user->password)){
-    //         return response()->json([
-    //             'success' => false,
-    //             'message' => 'Email atau password salah'
-    //         ], 401);
-    //     }
-        
-    // }
+    public function login(Request $request){
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+        $user = UserMobile::where('email', $request->email)->first();
+        if(! $user || ! Hash::check($request->password, $user->password)){
+            return response()->json([
+                'success' => false,
+                'message' => 'Email atau password salah'
+            ], 401);
+        }
+        $user = UserMobile::where('email', $request['email'])->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()
+            ->json(['message' => 'Selamat Datang '.$user->nama_pelamar.', di Aplikasi Go Job','access_token' => $token, 'token_type' => 'Bearer', ]);
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\LoginMobileController;
 use App\Http\Controllers\API\RegisterMobileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -23,3 +24,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 //API route for register new user
 Route::post('/register', [RegisterMobileController::class, 'register']);
+
+//API route for login user
+Route::post('/login', [LoginMobileController::class, 'login']);
