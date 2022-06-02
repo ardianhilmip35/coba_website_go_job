4a330be6996889c0f36c496060098894bd34387b
ardianhilmip35 <ardianhil86@gmail.com>
2022-06-02 21:33:47 +0700

    hilmi commit

--- routes/api.php
+++ routes/api.php
@@ -48,3 +48,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     Route::post('/lowongan/lamar', [LowonganController::class, 'lamar']);
     
     });
+
+    //Yang dicobak dulu lihat lowongan dan perusahaan, middlewarenya matikan dulu
