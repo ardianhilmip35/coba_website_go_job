8a16888c1e9d389baf4642600015bbf3bef1e8f5
M-Ilham-Nugraha-Syahputra <80625504+M-Ilham-Nugraha-Syahputra@users.noreply.github.com>
2022-06-08 18:53:29 +0700

    ilham

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\lowongan;
 use App\Models\lamarkerja;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -27,6 +28,14 @@ class HomeController extends Controller
     {
         $jumlah_lowongan = lowongan::where('users_id', Auth::user()->id)->count();
         $jumlah_lamarkerja = lamarkerja::where('pelamar_id', Auth::user()->id)->firstOrFail()->count();
-        return view('dashboard.dashboardadm')->with('jumlah_lowongan', $jumlah_lowongan)->with('jumlah_lamarkerja', $jumlah_lamarkerja);
+
+        $total_pelamar = lamarkerja::select(DB::raw("CAST(SUM(id) as int) as id"))
+        ->GroupBy(DB::raw("MONTH(created_at)"))
+        ->pluck('id');
+        
+        $bulan = lamarkerja::select(DB::raw("MONTHNAME(created_at) as bulan"))->orderBy('created_at', 'asc')
+        ->GroupBy(DB::raw("MONTHNAME(created_at)"))
+        ->pluck('bulan');
+        return view('dashboard.dashboardadm',compact('total_pelamar','bulan'))->with('jumlah_lowongan', $jumlah_lowongan)->with('jumlah_lamarkerja', $jumlah_lamarkerja);
     }
 }
