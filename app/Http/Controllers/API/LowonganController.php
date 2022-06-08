8f46b9e8038cdb46614d15b2b90d02981fb5d01d
ardianhilmip35 <ardianhil86@gmail.com>
2022-06-09 06:23:10 +0700

    hilmi commit

--- app/Http/Controllers/API/LowonganController.php
+++ app/Http/Controllers/API/LowonganController.php
@@ -34,28 +34,15 @@ class LowonganController extends Controller
             'portofolio_pelamar' => 'required|mimes:pdf'
         ]);
 
-        $id_pelamar = UserMobile::where('id',   $request->user()->id)->get();
-        $validatedData['pelamar_id'] = $id_pelamar->id;
+        $validatedData['pelamar_id'] = $request->pelamar_id;
         $validatedData['portofolio_pelamar'] = $request->file('portofolio_pelamar')->move('img/uploads/lamaran', $request->file('portofolio_pelamar')->getClientOriginalName());
-
-        
         $create = lamarkerja::create($validatedData);
 
-        if($create){
-
-            return response()->json(['status' => 'success', 'message' => 'Lamaran berhasil dikirim']);
-        }    
-        else{
-            return response()->json(['status' => 'error', 'message' => 'Lamaran gagal dikirim']);
-        }
+        return response()->json($create);
     }
 
     public function lihatlamar(Request $request){
         $data = lamarkerja::where('pelamar_id', $request->user()->id)->get();
-        $data = $data->map(function($item){
-            $item->lowongan = lowongan::where('id', $item->lowongan_id)->get();
-            return $item;
-        });
         return response()->json($data);
     }
 
