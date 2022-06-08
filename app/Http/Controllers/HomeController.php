06090b9698beca3c5e1fcac30278234895cb60e9
M-Ilham-Nugraha-Syahputra <80625504+M-Ilham-Nugraha-Syahputra@users.noreply.github.com>
2022-06-08 14:30:54 +0700

    ilham

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use App\Models\lowongan;
 use App\Models\lamarkerja;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -24,8 +25,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $jumlah_lowongan = lowongan::all()->count();
-        $jumlah_lamarkerja = lamarkerja::all()->count();
+        $jumlah_lowongan = lowongan::where('users_id', Auth::user()->id)->count();
+        $jumlah_lamarkerja = lamarkerja::where('pelamar_id', Auth::user()->id)->firstOrFail()->count();
         return view('dashboard.dashboardadm')->with('jumlah_lowongan', $jumlah_lowongan)->with('jumlah_lamarkerja', $jumlah_lamarkerja);
     }
 }
